620024729335f3d6734d2b03903b788b2dbf8925
Kiane L. Casagrande <kiane.casagrande@gmail.com>
2018-12-09 19:18:10 -0200

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Como você pode ver, nós criamos uma função (def) chamada 
 # post_list que recebe um request, executa a função render 
 # que irá renderizar (montar) nosso modelo de acordo com o 
 # template blog/post_list.html, e retorna (return) o resultado.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
