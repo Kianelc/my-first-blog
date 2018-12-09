baf86ff53ea73e06b67b8206542d603bdb85e70b
Kiane L. Casagrande <kiane.casagrande@gmail.com>
2018-12-09 18:33:37 -0200

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Como você pode ver, nós criamos uma função (def) chamada 
+# post_list que recebe um request, executa a função render 
+# que irá renderizar (montar) nosso modelo de acordo com o 
+# template blog/post_list.html, e retorna (return) o resultado.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
